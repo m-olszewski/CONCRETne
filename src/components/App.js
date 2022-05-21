4a6f544300e0eebb12004b65cd1fb080d79cae25
m-olszewski <michal.e.olszewski@gmail.com>
2022-05-21 23:50:13 +0200

    opening

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import logo from '../logo.svg';
 import '../scss/_App.scss';
 import Header from "./Header";
+import Opening from "./Opening";
+import About from "./About";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Opening/>
+            <About/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Opening.js
@@ -0,0 +1,14 @@
+import '../scss/_Opening.scss'
+
+const Opening = () => {
+    return (
+        <div className="App-opening">
+            <div className="opening-box ">
+                <h1 className="opening-title">Idealny. Dla <strong>Ciebie</strong>.</h1>
+                <p className="opening-text">Wysoka jakość dopasowana do <strong>Twoich</strong> potrzeb.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Opening;
