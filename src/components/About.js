ebb8b84c80e9fb42fff37a4858c3164079544782
m-olszewski <michal.e.olszewski@gmail.com>
2022-05-21 23:50:22 +0200

    about

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import '../scss/_About.scss'
+
+const About = () => {
+    return (
+        <div className="App-about container">
+            <article className="about-article">
+                <div className="about-box">
+                    <p className="about-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius odio odit omnis sequi vel voluptatum.</p>
+                </div>
+
+                <img src="../assets/images/marble.jpg" alt="marble"/>
+            </article>
+
+            {/*<article className="about-article">*/}
+            {/*    <img src="../assets/images/wood.jpg" alt="wood"/>*/}
+
+            {/*    <div className="about-box">*/}
+            {/*        <p className="about-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius odio odit omnis sequi vel voluptatum.</p>*/}
+            {/*    </div>*/}
+            {/*</article>*/}
+
+            {/*<article className="about-article">*/}
+            {/*    <div className="about-box">*/}
+            {/*        <p className="about-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius odio odit omnis sequi vel voluptatum.</p>*/}
+            {/*    </div>*/}
+
+            {/*    <img src="../assets/images/gold.jpg" alt="gold"/>*/}
+            {/*</article>*/}
+        </div>
+    );
+};
+
+export default About;
