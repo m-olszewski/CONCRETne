dfd03dab7cfd7012e285867b16e38b488f1280d3
m-olszewski <michal.e.olszewski@gmail.com>
2022-05-22 12:29:32 +0200

    product

---
+++ src/components/Product.js
@@ -0,0 +1,24 @@
+import '../scss/_Product.scss'
+
+const Product = () => {
+    return (
+        <div className="App-product container">
+            <article className="product-article">
+                <div className="product-box">
+                    <p className="product-text">
+                        <strong>Marmur </strong>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi hic inventore molestiae? Ad commodi delectus dignissimos dolor illum molestiae nostrum odio porro, quaerat sint, unde vitae voluptas voluptate!
+                    </p>
+                </div>
+                <img src={require('../assets/images/marble.jpg')} alt="marble"/>
+            </article>
+            <div className="product-nav">
+                <span className="product-nav-prev">prev</span>
+                <span className="product-nav-next">next</span>
+            </div>
+
+        </div>
+    );
+};
+
+export default Product;
