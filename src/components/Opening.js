57747cf9f72f779725c4662071452e7ac15f1e57
m-olszewski <michal.e.olszewski@gmail.com>
2022-05-24 22:05:41 +0200

    stolik

--- src/components/Opening.js
+++ src/components/Opening.js
@@ -2,12 +2,42 @@ import '../scss/_Opening.scss'
 
 const Opening = () => {
     return (
+<>
+
+
         <div className="App-opening">
+            <div className="maindiv">
+
+                <div className="front-left"/>
+                <div className="front-right"/>
+                <div className="rear-left"/>
+                <div className="rear-right"/>
+                <div className="front-left1"/>
+                <div className="front-right1"/>
+                <div className="rear-left1"/>
+                <div className="rear-right1"/>
+
+                <div className="up-right"/>
+                <div className="up-front"/>
+                <div className="top-top"/>
+                <div className="top-right"/>
+                <div className="top-front"/>
+
+                <div className="down-left"/>
+                <div className="down-right"/>
+                <div className="down-front"/>
+                <div className="down-rear"/>
+                <div className="down-left1"/>
+                <div className="down-right1"/>
+                <div className="down-front1"/>
+                <div className="down-rear1"/>
+
+            </div>
             <div className="opening-box ">
                 <h1 className="opening-title">Idealny. Dla <strong>Ciebie</strong>.</h1>
                 {/*<p className="opening-text">Wysoka jakość dopasowana do <strong>Twoich</strong> potrzeb.</p>*/}
             </div>
-        </div>
+        </div></>
     );
 };
 
